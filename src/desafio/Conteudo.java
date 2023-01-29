7dafdfb37cfde5c01cf8859339e5c14e26f1392e
Perkles <gabi.clezar@gmail.com>
2023-01-29 03:19:17 -0300

    Adicionando conteúdo do desafio

---
+++ src/desafio/Conteudo.java
@@ -0,0 +1,28 @@
+package desafio;
+
+public abstract class Conteudo {
+
+    protected static final double XP_PADRAO = 10d;
+
+    private String titulo;
+    private String descrição;
+
+    public abstract double calcularXp();
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescrição() {
+        return descrição;
+    }
+
+    public void setDescrição(String descrição) {
+        this.descrição = descrição;
+    }
+
+}
---
+++ src/desafio/Dev.java
@@ -0,0 +1,93 @@
+package desafio;
+
+import java.util.LinkedHashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscrito = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public void increverBootcamp (Bootcamp bootcamp) {
+        this.conteudosInscrito.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
+
+    public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscrito.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscrito.remove(conteudo.get());
+        }else {
+            System.out.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    }
+
+    public double calcularTotalXp () {
+       return this.conteudosConcluidos.stream().mapToDouble(conteudo -> conteudo.calcularXp()).sum();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscrito() {
+        return conteudosInscrito;
+    }
+
+    public void setConteudosInscrito(Set<Conteudo> conteudosInscrito) {
+        this.conteudosInscrito = conteudosInscrito;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((conteudosInscrito == null) ? 0 : conteudosInscrito.hashCode());
+        result = prime * result + ((conteudosConcluidos == null) ? 0 : conteudosConcluidos.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Dev other = (Dev) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (conteudosInscrito == null) {
+            if (other.conteudosInscrito != null)
+                return false;
+        } else if (!conteudosInscrito.equals(other.conteudosInscrito))
+            return false;
+        if (conteudosConcluidos == null) {
+            if (other.conteudosConcluidos != null)
+                return false;
+        } else if (!conteudosConcluidos.equals(other.conteudosConcluidos))
+            return false;
+        return true;
+    }
+    
+    
+}
